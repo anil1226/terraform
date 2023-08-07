17e966a490c4299ac2b52ce665fada67ce08c5d2
Anil Karuturi <anilchowdary1226@gmail.com>
2023-08-07 18:47:34 -0400

    tf modules tfplan

--- 49-Terraform-Modules-use-Public-Module/terraform-manifests/c5-virtual-network.tf
+++ 49-Terraform-Modules-use-Public-Module/terraform-manifests/c5-virtual-network.tf
@@ -37,27 +37,27 @@ module "vnet" {
 }
 
 # Create Public IP Address
-resource "azurerm_public_ip" "mypublicip" {
-  name                = local.pip_name
-  resource_group_name = azurerm_resource_group.myrg.name
-  location            = azurerm_resource_group.myrg.location
-  allocation_method   = "Static"
-  domain_name_label = "app1-${terraform.workspace}-${random_string.myrandom.id}"
-  tags = local.common_tags
-}
+# resource "azurerm_public_ip" "mypublicip" {
+#   name                = local.pip_name
+#   resource_group_name = azurerm_resource_group.myrg.name
+#   location            = azurerm_resource_group.myrg.location
+#   allocation_method   = "Static"
+#   domain_name_label = "app1-${terraform.workspace}-${random_string.myrandom.id}"
+#   tags = local.common_tags
+# }
 
 # Create Network Interface
-resource "azurerm_network_interface" "myvmnic" {
-  name                = local.nic_name
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+# resource "azurerm_network_interface" "myvmnic" {
+#   name                = local.nic_name
+#   location            = azurerm_resource_group.myrg.location
+#   resource_group_name = azurerm_resource_group.myrg.name
 
-  ip_configuration {
-    name                          = "internal"
-    #subnet_id                    = azurerm_subnet.mysubnet.id       
-    subnet_id                     = module.vnet.vnet_subnets[0]      
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.mypublicip.id 
-  }
-  tags = local.common_tags
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     #subnet_id                    = azurerm_subnet.mysubnet.id       
+#     subnet_id                     = module.vnet.vnet_subnets[0]      
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id = azurerm_public_ip.mypublicip.id 
+#   }
+#   tags = local.common_tags
+# }
