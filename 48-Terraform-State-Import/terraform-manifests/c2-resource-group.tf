a7ad8d7628aa62eead13544a2c479e329b099f3a
Anil Karuturi <anilchowdary1226@gmail.com>
2023-08-07 18:04:33 -0400

    tf import def tfplan

--- 48-Terraform-State-Import/terraform-manifests/c2-resource-group.tf
+++ 48-Terraform-State-Import/terraform-manifests/c2-resource-group.tf
@@ -1,14 +1,14 @@
 # terraform import azurerm_resource_group.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/example
 # terraform import azurerm_resource_group.example /subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RESOURCE_GROUP_NAME>
-# terraform import azurerm_resource_group.myrg /subscriptions/82808767-144c-4c66-a320-b30791668b0a/resourceGroups/myrg1
-/*
+# terraform import azurerm_resource_group.myrg /subscriptions/203f018b-3c4c-4749-af77-bccc924e8d77/resourceGroups/terraform-storage-rg
+
 # Resource Group
 resource "azurerm_resource_group" "myrg" {
-   name = "myrg1"
+   name = "terraform-storage-rg"
    location = "eastus"
    tags = {
      "Tag1" = "My-Tag-1"
    }
 }
-*/
+
 
